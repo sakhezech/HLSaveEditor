2b3b578ced48fba132b71e4e179967e09215445f
sakhezech <contact@sakhezech.ru>
2026-01-18 20:33:06 +0300

    remove current release maker

--- create_release.py
+++
@@ -1,7 +0,0 @@
-import os
-import PyInstaller.__main__
-
-if __name__ == "__main__":
-    PyInstaller.__main__.run([
-        "src/savemanager.py", "-w", "--collect-data", "glfw", "-F", "-n", "HLSaveManager", "-i", os.path.join('res', 'icon.ico'), "--add-data", f"{os.path.join('res', 'icon.ico')}{os.pathsep}res/."
-    ])
